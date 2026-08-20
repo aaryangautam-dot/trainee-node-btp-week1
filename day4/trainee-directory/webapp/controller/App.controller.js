e07683053a65222c630fdc7da0155cc2e642b49b
Aaryan <aaryangautam291@gmail.com>
2026-08-20 16:41:07 +0530

    SAPUI5 App Structure

---
+++ day4/trainee-directory/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("trainee.directory.traineedirectory.controller.App", {
+      onInit() {
+      }
+  });
+});
